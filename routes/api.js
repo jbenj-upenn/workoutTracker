14cbda461a3836534039e5009d063f4c49c89579
Joshua Benjamin <jbenj@sas.upenn.edu>
2020-06-28 12:21:20 -0400

    added get request in api routes

--- routes/api.js
+++ routes/api.js
@@ -1,7 +1,8 @@
 const router = require("express").Router();
 const db = require("../models/index.js");
+const mongoose = require("mongoose");
 
-router.post("/api/workout", ({ body }, res) => {
+router.post("/api/workouts", ({ body }, res) => {
     // console.log("testing...")
     // console.log(body)
   db.Workout.create(body)
@@ -17,36 +18,23 @@ router.post("/api/workout", ({ body }, res) => {
     });
 });
 
-router.post("/api/exercise", ({ body }, res) => {
-  db.Exercise.create(body)
-    .then(dbExercise => {
-      res.json(dbExercise);
-    })
-    .catch(err => {
-      res.status(400).json(err);
-    });
+router.get("/api/workouts", function (req, res) {
+    db.Workout.find({})
+        .populate("workouts")
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        });
 });
 
-
-// router.post("/api/transaction/bulk", ({ body }, res) => {
-//     Transaction.insertMany(body)
-//       .then(dbTransaction => {
-//         res.json(dbTransaction);
-//       })
-//       .catch(err => {
-//         res.status(400).json(err);
-//       });
-//   });
-  
-//   router.get("/api/transaction", (req, res) => {
-//     Transaction.find({})
-//       .sort({ date: -1 })
-//       .then(dbTransaction => {
-//         res.json(dbTransaction);
-//       })
-//       .catch(err => {
-//         res.status(400).json(err);
-//       });
-//   });
+/*======NO LONGER USING EXERCISE MODEL*/
+// router.post("/api/exercise", ({ body }, res) => {
+//   db.Exercise.create(body)
+//     .then(dbExercise => {
+//       res.json(dbExercise);
+//     })
+//     .catch(err => {
+//       res.status(400).json(err);
+//     });
+// });
   
   module.exports = router;
